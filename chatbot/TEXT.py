6c15976f0e9fbb393832d6aa40bf3921f4ec1a6e
Sankalp Kale <snkale1520@gmail.com>
2025-06-19 23:39:30 +0530

    added files and codes

---
+++ chatbot/TEXT.py
@@ -0,0 +1,6 @@
+{
+    "compile-hero.disable-compile-files-on-did-save-code": False,
+    "liveServer.settings.port": 5503,
+    "python.formatting.provider": "yapf",
+    "python.pythonPath": "C:\\ProgramData\\Anaconda3\\python.exe"
+}
